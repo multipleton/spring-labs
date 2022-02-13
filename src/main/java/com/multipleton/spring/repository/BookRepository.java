17952fa67a7e3f0eaffd889af5e933fe5f290511
Yehor Bublyk <yehor.bk@gmail.com>
2022-02-13 18:02:35 +0200

    repositories interfaced added

---
+++ src/main/java/com/multipleton/spring/repository/BookRepository.java
@@ -0,0 +1,18 @@
+package com.multipleton.spring.repository;
+
+import com.multipleton.spring.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository {
+
+    List<Book> findAll();
+
+    Optional<Book> findById(Long id);
+
+    Book save(Book book);
+
+    void deleteById(Long id);
+
+}
