18cdac70668ec5cbc60f823d6b3fd5398fc9eab8
Yehor Bublyk <yehor.bk@gmail.com>
2022-02-13 18:38:09 +0200

    dto added

---
+++ src/main/java/com/multipleton/spring/dto/author/AuthorCreateDto.java
@@ -0,0 +1,23 @@
+package com.multipleton.spring.dto.author;
+
+import com.multipleton.spring.model.Author;
+
+public class AuthorCreateDto {
+    private String name;
+
+    public AuthorCreateDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author toAuthor() {
+        return new Author(null, name);
+    }
+}
---
+++ src/main/java/com/multipleton/spring/dto/book/BookUpdateDto.java
@@ -0,0 +1,29 @@
+package com.multipleton.spring.dto.book;
+
+import com.multipleton.spring.model.Book;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BookUpdateDto {
+    private String tags;
+
+    public BookUpdateDto(String tags) {
+        this.tags = tags;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public void updateBook(Book book) {
+        Set<String> tags = Arrays.stream(this.tags.split(","))
+                        .collect(Collectors.toSet());
+        book.setTags(tags);
+    }
+}
