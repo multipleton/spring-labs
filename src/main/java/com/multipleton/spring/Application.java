f98accb71d051a83bc9a63890a71fd2e53dfb844
Yehor Bublyk <yehor.bk@gmail.com>
2022-02-17 09:05:49 +0200

    refactoring

--- src/main/java/com/multipleton/spring/Application.java
+++ src/main/java/com/multipleton/spring/Application.java
@@ -25,12 +25,26 @@ public class Application {
     CommandLineRunner stubRunner(AuthorRepository authorRepository, BookRepository bookRepository) {
         return args -> {
             List<Author> authors = new ArrayList<>();
-            authors.add(new Author(null, "Bob Dylan"));
+            authors.add(new Author(null, "Stephen King"));
+            authors.add(new Author(null, "Erich Maria Remarque"));
+            authors.add(new Author(null, "H. P. Lovecraft"));
             authors.forEach(authorRepository::save);
             List<Book> books = new ArrayList<>();
-            Set<String> tags = new HashSet<>();
-            tags.add("Horror");
-            books.add(new Book(null, "Doom", authors.get(0), tags));
+            Set<String> tags1 = new HashSet<>();
+            tags1.add("Horror");
+            tags1.add("Psychological horror");
+            tags1.add("Novel");
+            Set<String> tags2 = new HashSet<>();
+            tags2.add("War");
+            tags2.add("Concentration camp");
+            tags2.add("Novel");
+            Set<String> tags3 = new HashSet<>();
+            tags3.add("Horror");
+            tags3.add("Madness");
+            tags3.add("Fantasy");
+            books.add(new Book(null, "The Shining", authors.get(0), tags1));
+            books.add(new Book(null, "Spark of Life", authors.get(1), tags2));
+            books.add(new Book(null, "The Call of Cthulhu", authors.get(2), tags3));
             books.forEach(bookRepository::save);
         };
     }
