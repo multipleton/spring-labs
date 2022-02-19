13059825b533e629a1750ce1ba2d7a962b6d8556
lizzochek <littled685@gmail.com>
2022-02-19 13:09:16 +0200

    Lab1

---
+++ Lab1/SpringHelloWorldWeb/src/main/java/com/example/demo/SpringHelloWorldWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldWebApplication.class, args);
+	}
+
+}
